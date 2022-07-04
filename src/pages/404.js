88590198f84b1b1c20f8d1b8bdc0fcee4715d30c
kondelow <achmadrasidi@gmail.com>
2022-07-04 19:53:47 +0700

    add page not found

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,15 +5,15 @@ import styles from "styles/NotFound.module.css";
 const NotFound = () => {
   const router = useRouter();
   return (
-    <section class={styles.notfound}>
-      <div class="container-fluid">
-        <div class="row text-center">
-          <div class="col-md-12">
-            <h1 class={styles.pageTitle}>404</h1>
+    <section className={styles.notfound}>
+      <div className="container-fluid">
+        <div className="row text-center">
+          <div className="col-md-12">
+            <h1 className={styles.pageTitle}>404</h1>
             <h2>Whoops....</h2>
             <p>Page you're looking for not found.</p>
             <button
-              class={`${styles.upcomingButton} w-50 mb-5`}
+              className={`${styles.upcomingButton} w-50 mb-5`}
               onClick={() => {
                 router.push("/");
               }}
